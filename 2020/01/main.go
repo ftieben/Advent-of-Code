8f29f5d21c33d31ad5cddbb47ba0de98b47fc00d
Florian Tieben <ftiebe@gmail.com>
2020-12-12 13:43:10 +0100

    2020 01 finished

---
+++ 2020/01/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("input.txt")
+	check(err)
+	lines := strings.Split(string(dat), "\n")
+	// Assign cap to avoid resize on every append.
+	nums := make([]int, 0, len(lines))
+
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		n, err := strconv.Atoi(l)
+		check(err)
+		nums = append(nums, n)
+	}
+	testNumbers(nums)
+	//fmt.Print(string(dat))
+}
+
+func testNumbers(numbers []int) {
+	finished := false
+	for _, number1 := range numbers {
+		for _, number2 := range numbers {
+			summe := number1 + number2
+			if summe == 2020 {
+				fmt.Printf("number1=%d number2=%d result=%d\n", number1, number2, number1*number2)
+				finished = true
+			}
+			if finished {
+				break
+			}
+		}
+		if finished {
+			break
+		}
+	}
+}
